a5a68e54d6de087d188a1919a1d4b49a29abbcc7
Renukaradhya T B <renukaradhyapower@gmail.com>
2023-11-08 16:52:48 +0530

    csharp program to capture the window screenshot and collect the element then write to json file

---
+++ csharp/CaptureAutomationElement/Component/CaptureScreen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Drawing; // add System.Drawing in references..
+using System.Drawing.Imaging; // add System.Drawing in references..
+using System.Diagnostics;
+
+namespace CaptureAutomationElement
+{
+    class CaptureScreen
+    {
+        // import GetForegroundWindow() window API...
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        // import GetWindowThreadProcessId() window API...
+        [DllImport("user32.dll")]
+        public static extern int GetWindowThreadProcessId(IntPtr hWnd, out int ProcessId);
+
+        // import GetWindowRect() window API...
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECTANGLE rect);
+
+        // Defining the custom structural for storing rectBound value...
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECTANGLE
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        // Method to capture the screenshot and Store in the file...
+        public bool CaptureScreenShot(IntPtr windowId, Process filenameProcess)
+        {
+            RECTANGLE windowRect;
+
+            if (GetWindowRect(windowId, out windowRect))
+            {
+                int width = windowRect.Right - windowRect.Left;
+                int height = windowRect.Bottom - windowRect.Top;
+
+                using (Bitmap screenshot = new Bitmap(width, height, PixelFormat.Format32bppArgb)) // creating the width and height of the image
+                {
+                    using (Graphics graphics = Graphics.FromImage(screenshot)) // accessing to Bitmap created image
+                    {
+                        graphics.CopyFromScreen(windowRect.Left, windowRect.Top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy); //draw the image same as the screen appearing
+                    }
+                    screenshot.Save($@"C:\Users\Admin\Desktop\practice\company_program_dotnet\Automation_Element\CaptureAutomationElement\react\src\images\{filenameProcess.ProcessName}{filenameProcess.Handle}.jpeg", ImageFormat.Jpeg);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ csharp/CaptureAutomationElement/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Automation; // right click on References, then add Presentation Core property.
+using CaptureAutomationElement.Component;
+using System.Diagnostics; // add System in references.
+
+namespace CaptureAutomationElement
+{
+    internal class Program
+    {
+        // Import the GetForegroundWindow function from user32.dll
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow(); // this function return the activate window handle
+
+        // import GetWindowThreadProcessId to get process object, it gives the process id by taking handle...
+        [DllImport("user32.dll")]
+        public static extern int GetWindowThreadProcessId(IntPtr hWnd, out int ProcessId);// this function return the processid, by taking the handle
+        
+        
+        static void Main(string[] args)
+        {
+            // Assuming there is no activeWindow
+            IntPtr previous = IntPtr.Zero;
+
+            JsonWriter js = new JsonWriter(); // creating an object for JsonWriter class
+
+            CaptureScreen cs = new CaptureScreen(); // creating an object for CaptureScreen class 
+
+            int processId = 0;
+
+            while (true)
+            {
+                // to get activate window...
+                IntPtr foregroundWindowHandle = GetForegroundWindow();
+
+                if (foregroundWindowHandle != IntPtr.Zero && foregroundWindowHandle != previous)
+                {
+                    long startTime = DateTime.Now.Ticks; //staring time
+
+                    previous = foregroundWindowHandle;
+                    AutomationElement rootElement = AutomationElement.FromHandle(foregroundWindowHandle); //this will get the root element of the window
+                    // for AutomationElement class : add UIAutomationClient in references
+
+                    GetWindowThreadProcessId(foregroundWindowHandle, out processId); 
+
+                    Process process = Process.GetProcessById(processId); // getting the process object by using process id
+                    // for Process class : add System in references
+
+                    if (rootElement != null)
+                    {
+                        Console.WriteLine("Found AutomationElement for the foreground window:");
+                        Console.WriteLine("Name: " + rootElement.Current.Name);
+
+                        AutomationElementCollection children = rootElement.FindAll(TreeScope.Descendants, Condition.TrueCondition);// collecting all the UIElement by using root element
+                        // TreeScope.Descendants means accessing the children , grant children and great grant children and so on except the parent element
+                        // Condition.TrueCondition means true for all the element.
+
+                        Console.WriteLine($"Number of last Children: {children.Count}");
+
+
+                        if (js.MyJsonWriter(children, process))
+                        {
+                            Console.WriteLine("json written successfully...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("json error..............");
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("----------------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No AutomationElement found for the given handle.");
+                    }
+
+                    if (cs.CaptureScreenShot(foregroundWindowHandle, process))
+                    {
+                        Console.WriteLine("Image taken sucessfully...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("failed to take image...");
+                    }
+
+                    long endTime = DateTime.Now.Ticks; // time ends. 
+
+                    Console.WriteLine($"Time span for {process.ProcessName}: {(endTime - startTime) / 10000}");
+
+                }
+            }
+        }
+    }
+}
