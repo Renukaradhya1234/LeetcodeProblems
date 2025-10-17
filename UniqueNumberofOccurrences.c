3897acaddf6583ae01c0d0bc4c260422e2891305
Renukaradhya T B <renukaradhytb2001@gmail.com>
2025-10-17 21:52:22 +0530

    Unique Number of Occurance problem solution...

---
+++ UniqueNumberofOccurrences.c
@@ -0,0 +1,95 @@
+#include <stdbool.h>
+
+void swapElements(int* array, int first_index, int second_index) {
+    int temp = array[first_index];
+    array[first_index] = array[second_index];
+    array[second_index] = temp;
+}
+
+void setPivotValueToLast(int* array, int start_index, int end_index) {
+    int highest_index = 0;
+    int mean_index = 0;
+    int mid_index = start_index + (end_index-start_index)/2;
+    
+    
+    /// finding the hightest value index....
+    if (array[start_index] > array[mid_index] && array[start_index] > array[end_index]) {
+        highest_index = start_index;
+    } else if (array[mid_index] > array[start_index] && array[mid_index] > array[end_index]) {
+        highest_index = mid_index;
+    } else {
+        highest_index = end_index;
+    }
+    
+    
+    if (start_index == highest_index) {
+        if (array[mid_index] > array[end_index]) {
+            mean_index = mid_index;
+        } else {
+            mean_index = end_index;
+        }
+    } else if (mid_index == highest_index) {
+        if (array[start_index] > array[end_index]) {
+            mean_index = start_index;
+        } else {
+            mean_index = end_index;
+        }
+    } else {
+        if (array[start_index] > array[mid_index]) {
+            mean_index = start_index;
+        } else {
+            mean_index = mid_index;
+        }
+    }
+
+    if (end_index != mean_index) {
+        /// if mean value not in end of the partition...
+        swapElements(array, mean_index, end_index);
+    }
+    
+}
+
+void quickSort(int* array, int start_index, int end_index) {
+    if (start_index < end_index) {
+        setPivotValueToLast(array, start_index, end_index);
+        int pivot_value = array[end_index];
+        int pivot_index = start_index-1;
+        for (int index = start_index; index < end_index; index++) {
+            if (array[index] < pivot_value) {
+                swapElements(array, ++pivot_index, index);
+            }
+        }
+        swapElements(array, ++pivot_index, end_index);
+        quickSort(array, pivot_index+1, end_index); /// right half array....
+        quickSort(array, start_index, pivot_index-1); /// left half array....
+    }
+}
+
+bool uniqueOccurrences(int* arr, int arrSize) {
+    bool result = true;
+    if (arrSize > 1) {
+        quickSort(arr, 0, arrSize-1);
+        int count_reference[1001] = { 0 };
+        int count = 1;
+        int prev_value = arr[0];
+        for (int index = 1; index < arrSize; index++) {
+            if (arr[index] == prev_value) {
+                count++;
+            } else {
+                if (count_reference[count] == 0) {
+                    count_reference[count]++;
+                    count = 1;
+                    prev_value = arr[index];
+                } else {
+                    result = false;
+                    break;
+                }
+            }
+        }
+
+        if (count_reference[count] != 0) {
+            result = false;
+        }
+    }
+    return result;
+}
